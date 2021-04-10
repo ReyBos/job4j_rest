8c3416e792f15f2e9200975ae20f9b202e40cee0
reybos <andreybosiy@yandex.ru>
2021-04-10 21:31:06 +0300

    3. Авторизация JWT [#456114]

---
+++ url/src/main/java/ru/reybos/url/UrlApplication.java
@@ -0,0 +1,19 @@
+package ru.reybos.url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class UrlApplication {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlApplication.class, args);
+    }
+
+}
